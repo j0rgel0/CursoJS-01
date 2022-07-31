29fc26552ac211e704ef05c807c1bf92c5e9588c
Jorge Francisco López Valdez <jorge.lopezvz@udlap.mx>
2022-07-31 17:00:12 -0500

    use stric en Javascript

--- LECCIÓN14/14-01-ModoStrict.js
+++ LECCIÓN14/14-01-ModoStrict.js
@@ -1,5 +1,5 @@
 "use strict"
-//Se deben definir al principio del programa o al inicio de una función.
+//Se deben definir al principio del programa o al inicio de una función.git add
 
 //x = 10; //x is not defined 
 let y = 5;
