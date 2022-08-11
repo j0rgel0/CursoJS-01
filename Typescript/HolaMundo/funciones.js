c9f46fbeb9fda4fd54eb797c9b48938f7d90fc1a
Jorge Francisco López Valdez <jorge.lopezvz@udlap.mx>
2022-08-11 16:22:49 -0500

    Ejercicios de typescript

---
+++ Typescript/HolaMundo/funciones.js
@@ -0,0 +1,12 @@
+"use strict";
+var saludo = "HolaMundo desde TypeScript";
+saludo = "cambio";
+let numero = 10;
+let cualquiera;
+cualquiera = "cadena";
+cualquiera = 10;
+function saludar() {
+    console.log(saludo);
+    console.log(numero);
+}
+saludar();
---
+++ Typescript/HolaMundo/funciones.ts
@@ -0,0 +1,14 @@
+var saludo ="HolaMundo desde TypeScript"
+saludo = "cambio";
+
+let numero:number = 10;
+let cualquiera: any;
+cualquiera = "cadena";
+cualquiera = 10;
+
+function saludar(){
+    console.log(saludo);
+    console.log(numero);
+}
+
+saludar();
